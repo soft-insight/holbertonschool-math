1cef30b50b0a7f43eca7f28a9ea1c11819aa63ab
soft-insight <j0108rodriguez@gmail.com>
2022-03-21 10:45:13 -0500

    complex numbers

--- 0x00-math_complex/7-main.c
+++ 0x00-math_complex/7-main.c
@@ -12,8 +12,8 @@ int main(void)
     complex c1, c2, c3;
     c1.re=4;
     c1.im=3;
-    c2.re=0;
-    c2.im=0;
+    c2.re=2;
+    c2.im=1;
     display_complex_number(c1);
     display_complex_number(c2);
     division(c1,c2,&c3);
