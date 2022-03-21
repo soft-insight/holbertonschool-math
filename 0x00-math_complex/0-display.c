a582621e6e66d9f6941bf695558df801a328a309
soft-insight <j0108rodriguez@gmail.com>
2022-03-21 00:16:36 -0500

    display

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -11,11 +11,6 @@
 
 void display_complex_number(complex c)
 {
-
-	if (c.re == 0)
-		printf("%fi\n", c.im);
-	if (c.im == 0)
-		printf("%f\n", c.re);
 	if (c.im < 0)
 		printf("%f - %fi\n", c.re, -1 * c.im);
 	else
