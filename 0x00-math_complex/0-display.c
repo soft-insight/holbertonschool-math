1bbf42c66eefcb2c928fa7eadde62cb83957d5dd
soft-insight <j0108rodriguez@gmail.com>
2022-03-21 09:44:34 -0500

    complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -11,13 +11,6 @@
 
 void display_complex_number(complex c)
 {
-	if (c.im == 0)
-		printf("%.2f\n", c.re);
-	else if (c.re == 0)
-		printf("%.2fi\n", c.im);
-	else if (c.im < 0)
-		printf("%.2f - %.2fi\n", c.re, -1 * c.im);
-	else
-		printf("%.2f + %.2fi\n", c.re, c.im);
+	printf("%f + %fi\n", c.re, c.im);
 }
 
