2abc51ec8475caa4386b7cece5793aec8ffdc910
soft-insight <j0108rodriguez@gmail.com>
2022-04-19 12:17:48 -0500

    Simpson's Rule: when n is impar included

---
+++ 0x02-math_integrals_and_ode/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "simpson.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+    float a = 0.0;
+    float b = 1.0;
+    int steps = 4;
+    double simpon_integral;
+
+    simpon_integral = simpson(a, b, steps);
+    printf("The Simpson approximation of the Integral is: %.9f\n", simpon_integral);
+    printf("The exact value is:                           %.9f\n", 3.1415926535 / 4);
+    return (0);
+}
--- 0x02-math_integrals_and_ode/1-simpson.c
+++ 0x02-math_integrals_and_ode/1-simpson.c
@@ -3,12 +3,15 @@
 #include <stdio.h>
 
 /**
+ * simpson - calculates integrals by Simpson's rule
+ * @a: inferior limit
+ * @b: superior limit
+ * @steps: pasos
  *
- *
- *
+ * Return: the value of the numerical integral
  */
 
-double simpson(double a, double b, int steps )
+double simpson(double a, double b, int steps)
 {
 	double area;
 	double yL, yC, yR;
@@ -16,19 +19,33 @@ double simpson(double a, double b, int steps )
 	int i;
 
 	area = 0;
-	
-	steps = steps / 2 * 2;
-	printf("%d\n", steps / 2 * 2);
 
-	h = (b - a)/steps;
-	
-	for (i = 0; i < steps / 2; i++)
+	h = (b - a) / steps;
+
+	if (steps % 2 != 0)
 	{
-		yL = 1 / (1 + pow(a + 2 * i * h, 2) );
-		yR = 1 / (1 + pow(a + 2 * (i + 1) *  h, 2));
-		yC = 1 / (1 + pow(a + (2 * i + 1) * h, 2));
-		
-		area = h / 3  * ( yL + 4 * yC + yR) + area;
+		for (i = 0; i < steps / 2; i++)
+		{
+			yL = 1 / (1 + pow(a +  2 * i * h, 2));
+			yR = 1 / (1 + pow(a +  2 * (i + 1) *  h, 2));
+			yC = 1 / (1 + pow(a +  (2 * i + 1) * h, 2));
+			area = h / 3  * (yL + 4 * yC + yR) + area;
+		}
+			yR = 1 / (1 + pow(a +  steps *  h, 2));
+			yL = 1 / (1 + pow(a + (steps - 1) * h, 2));
+
+			area = h / 3  * (yR + yL) + area;
 	}
+	else
+	{
+		for (i = 0; i < steps / 2; i++)
+		{
+			yL = 1 / (1 + pow(a +  2 * i * h, 2));
+			yR = 1 / (1 + pow(a +  2 * (i + 1) *  h, 2));
+			yC = 1 / (1 + pow(a +  (2 * i + 1) * h, 2));
+			area = h / 3  * (yL + 4 * yC + yR) + area;
+		}
+	}
+
 	return (area);
 }
